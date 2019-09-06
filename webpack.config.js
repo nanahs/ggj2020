0d90629f1aaf09e8df35f96c7feb04b9f1ba2b44
Jake Quattrocchi <jake.quattrocchi@gmail.com>
2019-09-06 03:28:25 -0400

    consolidate configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,59 @@ const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (env, options) => ({
+const babelLoader = {
+  loader: "babel-loader",
+  options: { presets: ["@babel/preset-env"] }
+};
+
+const postCssLoader = mode => {
+  const productionPlugins = [
+    require("@fullhuman/postcss-purgecss")({
+      content: ["./src/**/*.js", "./src/**/*.elm", "./src/**/*.html"],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    }),
+    require("cssnano")
+  ];
+
+  return {
+    loader: "postcss-loader",
+    options: {
+      plugins: [
+        require("tailwindcss"),
+        require("postcss-preset-env"),
+        ...(mode === "production" ? productionPlugins : [])
+      ]
+    }
+  };
+};
+
+const elmWebpackLoader = mode => ({
+  loader: "elm-webpack-loader",
+  options: {
+    cwd: __dirname,
+    runtimeOptions: "-A128m -H128m -n8m",
+    debug: mode === "development",
+    optimize: mode === "production"
+  }
+});
+
+module.exports = (env, { mode }) => ({
   output: { filename: "[name].[contenthash].js" },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: babelLoader
+      },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: { config: { ctx: { mode: options.mode } } }
-          }
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", postCssLoader(mode)]
       },
       {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader: "elm-webpack-loader",
-        options: {
-          cwd: __dirname,
-          runtimeOptions: "-A128m -H128m -n8m",
-          debug: options.mode === "development",
-          optimize: options.mode === "production"
-        }
+        use: elmWebpackLoader(mode)
       }
     ]
   },
